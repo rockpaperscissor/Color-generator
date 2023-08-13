434919a3359dfb7a778c7b06507a895544b49285
GAYATHRY P VASUDEVAN <84952234+rockpaperscissor@users.noreply.github.com>
2023-08-13 20:05:05 +0530

    Add files via upload

---
+++ clr.js
@@ -0,0 +1,14 @@
+const getColor = () =>{
+    //hex code
+    const randomNumber = Math.floor(Math.random()*1677215);
+    const randomCode="#"+ randomNumber.toString(16);
+    document.body.style.backgroundColor=randomCode;
+    document.getElementById("color-code").innerHTML=randomCode;
+
+    navigator.clipboard.writeText(randomCode);
+}
+//event call
+document.getElementById("btn").addEventListener('click',getColor)
+
+//initial call
+getColor();
